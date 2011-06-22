f1c0d4c857eb385d42519ac77343ce6a5f77eecb
Aaron Mills <aaron@jailbreakcreative.com>
2011-06-21 20:29:31 -0400

    Rails Snake Tracking Genetics Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snaketracker::Application.config.secret_token = 'de02f4704d24d5aacfafc55d54c19c3899f565bc1a4bfa666fa8a69d5f593ce9f69936d86d290012b489defe3dc894ff925bec53e18778332915b056248ffe86'
